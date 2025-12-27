3ad24ee2b16845dcb871a7ec6b5422bbe792d53d
nayem181 <nayemimranhasan@gmail.com>
2025-12-27 16:38:21 +0600

    added the id of challenge details

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(
@@ -50,6 +50,15 @@ async function run() {
     //   see api challenges
     app.get("/challenges", async (req, res) => {
       const result = await challengesCollection.find().toArray();
+      res.send(result);
+    });
+
+    //   see challenges with id
+    app.get("/challenges/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await challengesCollection.findOne({
+        _id: new ObjectId(id),
+      });
       res.send(result);
     });
 
